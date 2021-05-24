555be51f5186884212b022f1666e36b75d82cb1d
kauer <kza0067@auburn.edu>
2021-05-23 22:44:41 -0500

    added a navbar and some styling.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import './App.css';
 import Home from './components/Home';
+import React, { Component } from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
 // import background from './images/images.png';
 
 
-function App(props) {
-  return (
-    // <div style={{ backgroundImage: `url(${background})` }}>
-    <div className="container d-flex justify-content-center" >
-      <div className="row">
-        <div className="d-flex justify-content-center">
-          <h1>My Portfolio Site</h1>
-        </div>
-        <Home />
-      </div>
-    </div>
-    //  </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      // <div style={{ backgroundImage: `url(${background})` }}>
+      <>
+        <nav className="navbar navbar-expand-lg">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li><Link to={'/'} className="nav-link">Home</Link></li>
+              <li><Link to={'/About'} className="nav-link">About</Link></li>
+              <li><Link to={'/Contact'} className="nav-link">Contact</Link></li>
+            </ul>
+          </div>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </>
+    );
+  }
+
 }
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,12 @@ import './Home.css'
 function Home() {
 
     return (
-        <div className="row" style={{marginTop: "150px" }}>
-            <div className="col-md-12">
-                <h1>Hello, World</h1>
-                <h3>My name is Kevin and my passion is innovation and creating solutions to difficult problems.</h3>
+        <div className="container">
+            <div className="row" style={{ marginTop: "150px" }}>
+                <div className="col-md-12 home">
+                    <h1>Hello, World</h1>
+                    <h3>My name is Kevin and my passion is innovation and creating solutions to difficult problems.</h3>
+                </div>
             </div>
         </div>
     )
