d0635821009020c451b0b8dcf613f94e7f44f402
kauer <kza0067@auburn.edu>
2021-07-27 20:43:55 -0500

    Trying to create backend

--- Server/Server.js
+++ Server/Server.js
@@ -6,14 +6,13 @@ const PORT = process.env.PORT || 5000;
 //middleware
 app.use(express.static('public'));
 
-
-
-
-
-
-app.get('/', (req, res) => {
-    res.send('Hello World!!')
+app.get("/", (req, res) => {
+    res.sendFile("../public/index.js")
 })
+
+
+
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
